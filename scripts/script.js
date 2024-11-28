5631c5582266591487b572c5e4a4bcf96e19e699
pienachterberg <pien.achterberg@hva.nl>
2024-11-28 16:29:52 +0100

    nieuwe updates 28nov

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
 // JavaScript Document
 console.log("hi");
+
+var deButton = document.querySelector("section:nth-of-type(4) button");
+deButton.onclick = doeIets;
+
+function doeIets(){
+    this.classList.toggle("anders");
+}
+
+var deButtons = document.querySelectorAll('section button');
+var i = 0;
+while(i < deButtons.length) {
+    deButtons[i].onclick = doeIets;
+    i++;
+}
